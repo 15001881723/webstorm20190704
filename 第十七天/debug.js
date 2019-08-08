e505052e057f39b1a2d24de5c9a17a086eb0db81
bashila <626790649@qq.com>
2019-08-08 21:48:36 +0800

    999

--- 第十七天/debug.js
+++ 第十七天/debug.js
@@ -1,12 +1,13 @@
-function debug(msg) {
-    var log=document.getElementById("debuglog");
-    if (!log){
-        log=document.createElement("div");
-        log.id="debuglog";
-        document.body.appendChild(log);
+    function debug(msg) {
+        var log=document.getElementById("debuglog");
+        if (!log){
+            log=document.createElement("div");
+            log.id="debuglog";
+            log.innerHTML="<h1>初始内容</h1>"
+            document.body.appendChild(log);
+        }
+        var pre=document.createElement("pre");
+        var text=document.createTextNode(msg);
+        pre.appendChild(text);
+        log.appendChild(pre);
     }
-    var pre=document.createElement("pre");
-    var text=document.createTextNode(msg);
-    pre.appendChild(text);
-    log.appendChild(pre);
-}
