96e0fad82f31c1ac8c6224072e8e146c99013013
CD-41965\Administrator <626790649@qq.com>
2019-08-23 16:47:40 +0800

    456

--- jQuery基础教程学习/jQueryExample/02/02.js
+++ jQuery基础教程学习/jQueryExample/02/02.js
@@ -1,10 +1,27 @@
-// This is the custom JavaScript file referenced by index.html. You will notice
-// that this file is currently empty. By adding code to this empty file and
-// then viewing index.html in a browser, you can experiment with the example
-// page or follow along with the examples in the book.
-//
-// See README.txt for more information.
-  $(document).ready(function () {
-      $('#selected-plays>li').addClass('horizontal');
-      $('#selected-plays li:not(.horizontal)').addClass('sub-level');
-  });
+    // This is the custom JavaScript file referenced by index.html. You will notice
+    // that this file is currently empty. By adding code to this empty file and
+    // then viewing index.html in a browser, you can experiment with the example
+    // page or follow along with the examples in the book.
+    //
+    // See README.txt for more information.
+    $(document).ready(function () {
+        $('#selected-plays>li').addClass('horizontal');
+        $('#selected-plays li:not(.horizontal)').addClass('sub-level');
+    });
+    $(document).ready(function () {
+        $('tr:even').addClass('alt');
+        $('tr:nth-child(odd)').addClass('alt');
+        // $('td:contains(Henry)').addClass('highlight');
+        // $('td:contains(Henry)').nextAll().addClass('highlight');
+        // $('td:contains(Henry)').nextAll().addBack().addClass('highlight');// 第一种写法
+        // $('td:contains(Henry)').parent().children().addClass('bigChange')//第二种方法
+        $('a').filter(function () {
+
+            return this.hostname&&this.hostname!=location.hostname;
+
+        }).addClass('external');
+        //第三种方法
+        $('td:contains(Henry)').parent().find('td:eq(1)').addClass('bigChange').end()
+            .find('td:eq(2)').addClass('bigChange');
+        console.log( $('td:contains(Henry)').parent().find('td:eq(1)'))
+    })
--- jQuery基础教程学习/jQueryExample/03/03.js
+++ jQuery基础教程学习/jQueryExample/03/03.js
@@ -1,6 +1,20 @@
-// This is the custom JavaScript file referenced by index.html. You will notice
-// that this file is currently empty. By adding code to this empty file and
-// then viewing index.html in a browser, you can experiment with the example
-// page or follow along with the examples in the book.
-//
-// See README.txt for more information.
+    // This is the custom JavaScript file referenced by index.html. You will notice
+    // that this file is currently empty. By adding code to this empty file and
+    // then viewing index.html in a browser, you can experiment with the example
+    // page or follow along with the examples in the book.
+    //
+    // See README.txt for more information.
+   $(function () {
+     $('#switcher-large').on('click',function () {
+         $('body').addClass('large');
+         $('body').removeClass('narrow');
+     })
+       $('#switcher-narrow').on('click',function () {
+           $('body').addClass('narrow');
+           $('body').removeClass('large');
+       })
+       $('#switcher-default').on('click',function () {
+           $('body').removeClass('narrow');
+           $('body').removeClass('large');
+       })
+   })
