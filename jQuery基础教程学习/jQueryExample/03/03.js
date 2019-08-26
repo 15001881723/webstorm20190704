a47b7bda05764493f543a4ccd50766b4594c2b04
CD-41965\Administrator <626790649@qq.com>
2019-08-26 16:40:34 +0800

    102

--- jQuery基础教程学习/jQueryExample/03/03.js
+++ jQuery基础教程学习/jQueryExample/03/03.js
@@ -4,17 +4,60 @@
     // page or follow along with the examples in the book.
     //
     // See README.txt for more information.
-   $(function () {
-     $('#switcher-large').on('click',function () {
-         $('body').addClass('large');
-         $('body').removeClass('narrow');
-     })
-       $('#switcher-narrow').on('click',function () {
-           $('body').addClass('narrow');
-           $('body').removeClass('large');
-       })
-       $('#switcher-default').on('click',function () {
-           $('body').removeClass('narrow');
-           $('body').removeClass('large');
-       })
-   })
+   // $(function () {
+   //   $('#switcher-large').on('click',function () {
+   //       $('body').addClass('large');
+   //       $('body').removeClass('narrow');
+   //   })
+   //     $('#switcher-narrow').on('click',function () {
+   //         $('body').addClass('narrow');
+   //         $('body').removeClass('large');
+   //     })
+   //     $('#switcher-default').on('click',function () {
+   //         $('body').removeClass('narrow');
+   //         $('body').removeClass('large');
+   //     })
+   // })
+
+    // $(function () {
+    //     $('#switcher-large')
+    //         .on('click',function () {
+    //         $('body').addClass('large');
+    //         $('body').removeClass('narrow');
+    //             $('#switcher button').removeClass('selected');
+    //             $(this).addClass('selected')
+    //     })
+    //     $('#switcher-narrow').on('click',function () {
+    //         $('body').addClass('narrow');
+    //         $('body').removeClass('large');
+    //         $('#switcher button').removeClass('selected');
+    //         $(this).addClass('selected')
+    //     })
+    //     $('#switcher-default').
+    //         addClass('selected').on('click',function () {
+    //         $('body').removeClass('narrow');
+    //         $('body').removeClass('large');
+    //         $('#switcher button').removeClass('selected');
+    //         $(this).addClass('selected')
+    //     })
+    // })
+
+    //终极写法
+    $(document).ready(function () {
+        $('#switcher-default').addClass('selected');
+        $('#switcher button').on('click',function () {
+            var bodyClass=this.id.split('-')[1];
+            $('body').removeClass().addClass(bodyClass);
+            $('#switcher button').removeClass();
+            $(this).addClass('selected');
+        })
+        $('#switcher h3').click(function () {
+            $('#switcher button').toggleClass('hidden')
+        })
+        $('#switcher h3').hover(function () {
+            $(this).addClass('hover')
+        },function () {
+            $(this).removeClass('hover')
+        })
+
+    })
