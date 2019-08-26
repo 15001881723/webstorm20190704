0e22ebecb706ed25f6d01bead5f64e7173d2146b
bashila <626790649@qq.com>
2019-08-26 22:21:21 +0800

    1234

--- jQuery基础教程学习/jQueryExample/03/03.js
+++ jQuery基础教程学习/jQueryExample/03/03.js
@@ -43,21 +43,64 @@
     // })
 
     //终极写法
+    // $(document).ready(function () {
+    //     $('#switcher-default').addClass('selected');
+    //     $('#switcher button').on('click',function () {
+    //         var bodyClass=this.id.split('-')[1];
+    //         $('body').removeClass().addClass(bodyClass);
+    //         $('#switcher button').removeClass();
+    //         $(this).addClass('selected');
+    //     })
+    //     $('#switcher h3').click(function () {
+    //         $('#switcher button').toggleClass('hidden')
+    //     })
+    //     $('#switcher h3').hover(function () {
+    //         $(this).addClass('hover')
+    //     },function () {
+    //         $(this).removeClass('hover')
+    //     })
+    //
+    // })
+    //这是点击空白区域按钮折叠
+    // $(document).ready(function () {
+    //     $('#switcher').click(function (event) {
+    //         if(event.target==this){
+    //             $('#switcher button').toggleClass('hidden')
+    //         }
+    //
+    //
+    //     })
+    // })
+
+    //有一种写法是对上述两种不同操作反应的综合写法
+    // $(document).ready(function () {
+    //     $('#switcher').click(function (event) {
+    //
+    //             $('#switcher button').toggleClass('hidden')
+    //     })
+    // })
+//下面程序有两种写法，第一种是 采用jQuery内部遍历的方法
+//     $(document).ready(function () {
+//         $('#switcher-default').addClass('selected');
+//         $('#switcher button').on('click',function () {
+//             var bodyClass=this.id.split('-')[1];
+//             $('body').removeClass().addClass(bodyClass);
+//             $('#switcher button').removeClass();
+//             $(this).addClass('selected');
+//             event.stopPropagation();
+//         })
+//     })
+    //下面程序有两种写法，第二种采用的是事件委托的方法，事件委托采用的是事件冒泡的策略,通过条件判使通过条件的事件才能冒上来
     $(document).ready(function () {
         $('#switcher-default').addClass('selected');
-        $('#switcher button').on('click',function () {
-            var bodyClass=this.id.split('-')[1];
-            $('body').removeClass().addClass(bodyClass);
-            $('#switcher button').removeClass();
-            $(this).addClass('selected');
-        })
-        $('#switcher h3').click(function () {
-            $('#switcher button').toggleClass('hidden')
-        })
-        $('#switcher h3').hover(function () {
-            $(this).addClass('hover')
-        },function () {
-            $(this).removeClass('hover')
-        })
+        $('#switcher').on('click',function (event) {
+            if($(event.target).is('button')){   //条件判断属性是不是button
+                var bodyClass=event.target.id.split('-')[1];
+                $('body').removeClass().addClass(bodyClass);
+                $('#switcher button').removeClass('selected');
+                $(event.target).addClass('selected');
+                event.stopPropagation();
+            }
 
+        })
     })
