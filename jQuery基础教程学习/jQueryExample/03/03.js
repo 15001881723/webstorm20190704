bca9356039a39b309947ff5532fc6d9885c221e2
CD-41965\Administrator <626790649@qq.com>
2019-08-27 16:42:27 +0800

    1213

--- jQuery基础教程学习/jQueryExample/03/03.js
+++ jQuery基础教程学习/jQueryExample/03/03.js
@@ -44,16 +44,16 @@
 
     //终极写法
     // $(document).ready(function () {
-    //     $('#switcher-default').addClass('selected');
-    //     $('#switcher button').on('click',function () {
-    //         var bodyClass=this.id.split('-')[1];
-    //         $('body').removeClass().addClass(bodyClass);
-    //         $('#switcher button').removeClass();
-    //         $(this).addClass('selected');
-    //     })
-    //     $('#switcher h3').click(function () {
-    //         $('#switcher button').toggleClass('hidden')
-    //     })
+    //     // $('#switcher-default').addClass('selected');
+    //     // $('#switcher button').on('click',function () {
+    //     //     var bodyClass=this.id.split('-')[1];
+    //     //     $('body').removeClass().addClass(bodyClass);
+    //     //     $('#switcher button').removeClass();
+    //     //     $(this).addClass('selected');
+    //     // })
+    //     // $('#switcher h3').click(function () {
+    //     //     $('#switcher button').toggleClass('hidden')
+    //     // })
     //     $('#switcher h3').hover(function () {
     //         $(this).addClass('hover')
     //     },function () {
@@ -91,16 +91,73 @@
 //         })
 //     })
     //下面程序有两种写法，第二种采用的是事件委托的方法，事件委托采用的是事件冒泡的策略,通过条件判使通过条件的事件才能冒上来
-    $(document).ready(function () {
-        $('#switcher-default').addClass('selected');
-        $('#switcher').on('click',function (event) {
-            if($(event.target).is('button')){   //条件判断属性是不是button
-                var bodyClass=event.target.id.split('-')[1];
-                $('body').removeClass().addClass(bodyClass);
-                $('#switcher button').removeClass('selected');
-                $(event.target).addClass('selected');
-                event.stopPropagation();
-            }
+    // $(document).ready(function () {
+    //     $('#switcher-default').addClass('selected');
+    //     $('#switcher').on('click',function (event) {
+    //         if($(event.target).is('button')){   //条件判断属性是不是button
+    //             var bodyClass=event.target.id.split('-')[1];
+    //             $('body').removeClass().addClass(bodyClass);
+    //             $('#switcher button').removeClass('selected');
+    //             $(event.target).addClass('selected');
+    //             // event.stopPropagation();
+    //         }else {
+    //             $('#switcher button').toggleClass('hidden');
+    //         }
+    //
+    //     })
+    // })
+    //内置事件委托功能
+    // $(document).ready(function () {
+    //     $('#switcher').on('click','button',function () { //将#switcher下的button绑定click事件
+    //         console.log("我是内置事件委托方法.on()")
+    //     })
+    // })
+    //移除事件处理程序
+    // $(document).ready(function () {
+    //     // $('#switcher-default').addClass('selected');
+    //     $('#switcher').on('click.collapse',function (event) {
+    //         if(!$(event.target).is('button')){   //条件判断属性是不是button
+    //
+    //             $('#switcher button').toggleClass('hidden');
+    //         }
+    //
+    //     })
+    //     $('#switcher-narrow,#switcher-large').click(function () {
+    //         $('#switcher').off('click.collapse')
+    //     })
+    //
+    // })
 
-        })
+    //换一种简写的方式
+    // $(document).ready(function () {
+    //    var switcherToggle=function (event) {
+    //        if(!$(event.target).is('button')){   //条件判断属性是不是button
+    //            $('#switcher button').toggleClass('hidden');
+    //        }
+    //    }
+    //     $('#switcher').on('click.collapse',switcherToggle);
+    //     $('#switcher-narrow,#switcher-large').click(function () {
+    //         $('#switcher').off('click.collapse')
+    //     })
+    //     $('#switcher-default').click(function () {
+    //         $('#switcher').on('click.collapse',switcherToggle)
+    //     })
+    // })
+
+    //上述代码有一个bug，就是当switch-default点击次数为偶数次的时候，那么将会失效，所以需要一种解决方案，用户在单击任何按钮时解除绑定，在确定单击按钮
+    //是在switch-default 这个id下的重新绑定
+    $(document).ready(function () {
+       var switcherToggle=function (event) {
+           if(!$(event.target).is('button')){   //条件判断属性是不是button
+               $('#switcher button').toggleClass('hidden');
+           }
+       }
+        $('#switcher').on('click.collapse',switcherToggle);
+        $('#switcher button').click(function () {
+            $('#switcher').off('click.collapse');
+            if(this.id==='switcher-default'){
+           $('#switcher').on('click.collapse',switcherToggle);
+        }
+
+    })
     })
