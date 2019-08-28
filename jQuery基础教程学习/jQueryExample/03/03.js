67c8ace3fe1d5a493feaba920de0949810d66cde
CD-41965\Administrator <626790649@qq.com>
2019-08-28 16:44:39 +0800

    4563

--- jQuery基础教程学习/jQueryExample/03/03.js
+++ jQuery基础教程学习/jQueryExample/03/03.js
@@ -175,4 +175,82 @@
 
         })
     })
-    //第三题
+    //第三题 向右的方向键码是39 按下的时候切换到下一个body类
+//     $(document).ready(function () {
+//             var setBodyClass = function(className) {
+//                 $('body').removeClass().addClass(className);
+//                 $('#switcher button').removeClass('selected');
+//                 $('#switcher-' + className).addClass('selected');
+//             };
+//         $('#switcher-default').addClass('selected');
+//         var bodyClass=['default','narrow','large'];
+//         var count=0;
+//         $(document).keyup(function (event) {
+//             if (event.keyCode===39){
+//                 var num=(count+1)%3
+//                 count++;
+//                 setBodyClass(bodyClass[num]);
+//             }
+//         })
+//
+// })
+
+    $(document).ready(function () {
+        var setBodyClass=function(className){
+            $("body").removeClass().addClass(className);
+            $(".switcher button").removeClass("selected");
+            $("#switcher-"+className).addClass("selected");
+        }
+        $("#switcher-default").addClass("selected");//页面初始时默认为选中状态
+        $(document).keyup(function (event) {
+            var num=$(".switcher button").filter(".selected").index()-1;
+            console.log("num1是"+num)
+            if(event.which==39) num=(num+1)%3;
+            else if(event.which==37) num=(num-1)%3;
+            else return false;
+            var btnClassname=$(".switcher button").eq(num).attr("id").split("-")[1];//.eq(index)  index整数，指示元素的位置（最小为 0）。
+                                                                                    // 如果是负数，则从集合中的最后一个元素往回计数
+            setBodyClass(btnClassname);
+            console.log($(".switcher button").filter(".selected").index())
+            console.log("num是"+num)
+            console.log("(num-1)%3;是"+(num-1)%3)
+            console.log($(".switcher button").eq(num))
+        })
+
+
+
+    })
+
+    //4、挑战：使用console.log()函数记录在段落中移动的鼠标的坐标位置。
+    $(function(){
+        $(document).mouseover(function(event) {
+            console.log(event.pageX);
+            console.log(event.pageY);
+        })
+    })
+
+
+    // 5、挑战：使用.mousedown()和.mouseup()跟踪页面中的鼠标事件。如果鼠标按键在按下它的地方被释放，
+    // 则为所有段落添加hidden类。如果是在按下它的地方之下被释放的，删除所有段落的hidden类。(这
+
+    // $(function () {
+    //   var x_down,y_down,x_up,y_up;
+    //   $(document).mousedown(function (event) {
+    //       x_down=event.clientX;
+    //       y_down=event.clientY;
+    //   })
+    //
+    //     $(document).mouseup(function () {
+    //         x_up=event.clientX;
+    //         y_up=event.clientY;
+    //
+    //         if (x_up===x_down|| y_up===y_down){
+    //             $('p').addClass('hidden')
+    //         }else{
+    //             $('p').removeClass('hidden')
+    //         }
+    //     })
+    // })
+
+
+
--- jQuery基础教程学习/jQueryExample/04/04.js
+++ jQuery基础教程学习/jQueryExample/04/04.js
@@ -1,6 +1,96 @@
-// This is the custom JavaScript file referenced by index.html. You will notice
-// that this file is currently empty. By adding code to this empty file and
-// then viewing index.html in a browser, you can experiment with the example
-// page or follow along with the examples in the book.
-//
-// See README.txt for more information.
+    // This is the custom JavaScript file referenced by index.html. You will notice
+    // that this file is currently empty. By adding code to this empty file and
+    // then viewing index.html in a browser, you can experiment with the example
+    // page or follow along with the examples in the book.
+    //
+    // See README.txt for more information.
+
+    //代码清单4.1将文本区域speech放大40%
+    // $(function () {
+    // $('#switcher-large').on('click',function () {
+    //
+    //     var num=parseFloat($('.speech').css('fontSize'));
+    //     num=num*1.4;
+    //     $('.speech').css('fontSize',num+'px')
+    // })
+    // })
+    //代码清单4.2将文本区域speech放大40% 按钮small就缩小40% 两个功能
+    // $(function () {
+    //     $('#switcher button').on('click',function () {
+    //     var num=parseFloat($('.speech').css('fontSize'));
+    //     if(this.id==='switcher-large'){
+    //         num=num*1.4;
+    //         $('.speech').css('fontSize',num+'px')
+    //     }else if(this.id==='switcher-small'){
+    //         num=num/1.4;
+    //         $('.speech').css('fontSize',num+'px')
+    //     }
+    //
+    //     })
+    // })
+
+    //代码清单4.3将文本区域speech放大40% 按钮small就缩小40% 复原成默认状态 三个功能按钮都有作用
+
+    // $(function () {
+    //     var num1=parseFloat($('.speech').css('fontSize'));
+    //     $('#switcher button').on('click',function () {
+    //         var num=parseFloat($('.speech').css('fontSize'));
+    //         switch(this.id){
+    //             case 'switcher-large':
+    //                 num=num*1.4;
+    //                 $('.speech').css('fontSize',num+'px');
+    //                 break;
+    //             case 'switcher-small':
+    //                 num=num/1.4;
+    //                 $('.speech').css('fontSize',num+'px');
+    //                 break;
+    //             case 'switcher-default':
+    //                 $('.speech').css('fontSize',num1+'px');
+    //                 console.log('454545'+num1)
+    //                 break;
+    //         }
+    //
+    //
+    //
+    //
+    //     })
+    // })
+    //     $(function () {
+    //         $('p').eq(1).hide();
+    //         $('a.more').click(function (event)
+    //         {
+    //         event.preventDefault();
+    //             $('p').eq(1).show('slow');
+    //             $(this).hide('duration');
+    //         })
+    //     })
+
+        //代码清单4-9淡入淡出
+    // $(function () {
+    //     $('p').eq(1).hide();
+    //     $('a.more').click(function (event)
+    //     {
+    //         event.preventDefault();
+    //         $('p').eq(1).slideDown('slow');
+    //         $(this).hide('duration');
+    //     })
+    // })
+    //代码清单4-11多功能显示与隐藏
+    $(function () {
+        $('p').eq(1).hide();
+       var  $p1=$('p').eq(1);
+        $('a.more').click(function (event) {
+            event.preventDefault();
+            if($p1.is(':hidden')){
+                $('p').eq(1).fadeIn('slow');
+
+                $(this).text('read less')
+            }else{
+                $('p').eq(1).fadeOut('slow');
+
+                $(this).text('read more')
+            }
+
+
+        })
+    })
