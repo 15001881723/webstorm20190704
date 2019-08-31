7f455e8fae87a5289010195e202e3bb4df41096e
bashila <626790649@qq.com>
2019-08-31 22:03:25 +0800

    112233

--- jQuery基础教程学习/jQueryExample/05/05.js
+++ jQuery基础教程学习/jQueryExample/05/05.js
@@ -56,10 +56,68 @@
     // })
     //例 5-9 将每个段落的footnote找出来添加到文章footer与chapter之间 使用的技术是元素的移动 insertBefore()在现有元素外部，之前添加内容
     //并将sup插入到正文中
+    // $(function () {
+    //     var $note=$('<ol></ol>').insertBefore('#footer');
+    //     $('span.footnote').each(function (index) {
+    //         $('<sup>'+(index+1)+'</sup>').insertBefore(this);
+    //         $(this).appendTo($note).wrap('<li></li>');
+    //     })
+    // })
+    //例 5-16 将每个段落的footnote找出来添加到文章footer与chapter之间 使用的技术是元素的移动 insertBefore()在现有元素外部，之前添加内容
+    //并将sup插入到正文中
+    //对footnote与下面的内容进行连接 具体需求为 点击任何一方 都会指向另外一方
+    // $(function () {
+    //     var $note=$('<ol></ol>').insertBefore('#footer');
+    //     $('span.footnote').each(function (index) {
+    //       //反向插入用before  $('<sup>'+(index+1)+'</sup>').insertBefore(this);
+    //         $(this).before(['<sup>',(index+1),'</sup>'].join(''))
+    //                .appendTo($note)
+    //                .wrap('<li></li>');
+    //     })
+    // })
+    /*
+     先添加在脚注上a链接指向 下面的标注
+     */
+//     $(function () {
+    // //         var $note=$('<ol></ol>').insertBefore('#footer');
+    // //         $('span.footnote').each(function (index) {
+    // //             //反向插入用before  $('<sup>'+(index+1)+'</sup>').insertBefore(this);
+    // //             $(this).before([
+    // // '<a href="#footer-', index+1, '" id="context"', 'class="context">' ,'<sup>' ,(index+1) , '</sup></a>'
+    // //             ].join(''))
+    // //                 .appendTo($note)
+    // //                 .append([
+    // //                     '&nbsp;(<a href="#context-',
+    // //                     index+1,
+    // //                     '">context</a>)'//把context这个字符串反向添加到下面的li中 并为该字符串添加a链接
+    // //                 ].join(''))
+    // //                 .wrap('<li id="footer-'+(index+1)+'"></li>');
+    // //         })
+    // //     })
+    //例题5-18 对带有类pulled-quote 的p进行复制 然后添加类pulled 调整位置 做出突出引用的效果
+    // $(function () {
+    //     $('.pull-quote').each(function () {
+    //         var span_parent=$(this).parent('p');//指定找到p的父级元素
+    //         span_parent.css({position:'relative'});
+    //         var copy=$(this).clone();
+    //         copy.addClass('pulled').prependTo(span_parent)//元素.prependTo(位置) 将元素添加到位置
+    //
+    //     })
+    // })
+    //例题5-20 对带有类pulled-quote 的p进行复制 然后添加类pulled 调整位置 做出突出引用的效果
+    //新增需求 将突出显示的那个框的加粗 标记等特性去掉 使用的是.text()方法 该方法与.html()类似但是
+    //html（）不加内容是取得是元素本身的内容 而text获取是纯文本 在添加方法也是添加纯文本
     $(function () {
-        var $note=$('<ol></ol>').insertBefore('#footer');
-        $('span.footnote').each(function (index) {
-            $('<sup>'+(index+1)+'</sup>').insertBefore(this);
-            $(this).appendTo($note).wrap('<li></li>');
+        $('.pull-quote').each(function () {
+            var span_parent=$(this).parent('p');//指定找到p的父级元素
+            span_parent.css({position:'relative'});
+            var copy=$(this).clone();
+            copy.addClass('pulled')
+                .find('span.drop')
+                .html('&hellip;')
+                .end()
+                .text(copy.text())
+                .prependTo(span_parent)//元素.prependTo(位置) 将元素添加到位置
+
         })
     })
