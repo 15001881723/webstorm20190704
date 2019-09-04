3bde5c9e86d4d199bdebc32766337bd27cf9b1d1
CD-41965\Administrator <626790649@qq.com>
2019-09-04 16:37:50 +0800

    12121

--- jQuery1/jQueryExample/06/06.js
+++ jQuery1/jQueryExample/06/06.js
@@ -6,13 +6,13 @@
     // See README.txt for more information.
     
 // //例6-2 将html片段a.html添加到主页面中
-    $(function () {
-        $('#letter-a a').click(function (event) {
-            event.preventDefault();
-            $('#dictionary').load('a.html');
-            alert('loaded!!');
-        })
-    })
+//     $(function () {
+//         $('#letter-a a').click(function (event) {
+//             event.preventDefault();
+//             $('#dictionary').load('a.html');
+//
+//         })
+//     })
 //
 //     //例6-6 通过$.getJSON获取b.json，然后当这个json加载完成以后 利用回调函数
     $(function () {
@@ -102,13 +102,62 @@
     //6-10模拟向服务器请求 get 模拟服务器接口
     // 接口：在a链接绑定的事件上 这个 var requestData={ term: $(this).text()};
     //在li标签上取得term
+    // $(function () {
+    //     $('#letter-e a').click(function (event) {
+    //         event.preventDefault();
+    //         var requestData={term:$(this).text()}
+    //         $.get('e.php',requestData,function (data) {
+    //             $('#dictionary').html(data);
+    //         })
+    //         // $('#dictionary').load('e.php',requestData);
+    //     })
+    // })
+    //6-13/14 序列化表单使用jquery的ajax工具箱能够异步提交表单
+    //做两个input搜索框 然后再搜索框进行提交查询
+    $(function () {
+        $('#letter-f form').submit(function (event) {
+            event.preventDefault();
+            // $.get('f.php',{'term':$('input[name="term"]').val()},function (data) {
+            //     $('#dictionary').html(data);
+            // })
+
+            //上面写法也可以是这样的
+            var formValue=$(this).serialize();
+            $.get('f.php',formValue,function (data) {
+                $('#dictionary').html(data);
+            })
+        })
+    })
+    //6-15 ajax.start()   ajax.stop() 加载反馈系统
+    // ajax.start()请求开始还未进行其他传输  ajax.stop()最后一次请求活动中止
+    $(function () {
+        var $loading=$('<div id="loading">loading....</div>').insertBefore('#dictionary')
+        $(document).ajaxStart(function () {
+            $loading.show();
+        }).ajaxStop(function () {
+            $loading.hide();
+        })
+    })
+
+    //6-16 加载完成以后 触发回调函数 开始时候 #dictionary 是被隐藏的 加载完毕以后 回调函数以淡入淡出方式进行加载
+    // $(function () {
+    //     $('#letter-a a').click(function (event) {
+    //         event.preventDefault();
+    //         $('#dictionary').hide().load('a.html',function () {
+    //             $(this).fadeIn();
+    //         })
+    //     })
+    // })
+    //6-17错误处理可以使用ajaxError()的方法 但是此处使用的是jquery 延迟对象系统 fail（）
     $(function () {
         $('#letter-e a').click(function (event) {
             event.preventDefault();
-            var requestData={term:$(this).text()}
-            $.get('e.php',requestData,function (data) {
+            var formValue=$(this).serialize();
+            $.get('q.php',formValue,function (data) {
                 $('#dictionary').html(data);
+            }).fail(function (jqXHR) {
+                $('#dictionary').html('an error occour:'+jqXHR.status).append(jqXHR.responseText)
+
             })
-            // $('#dictionary').load('e.php',requestData);
         })
     })
