28f5672eb87240829384b1edc763164344c9ac81
CD-41965\Administrator <626790649@qq.com>
2019-09-11 16:40:54 +0800

    545

--- jQuery1/jQueryExample/08/08.js
+++ jQuery1/jQueryExample/08/08.js
@@ -5,44 +5,87 @@
     //
     // See README.txt for more information.
 
-    (function($) {
-        $.mathUtils = {
-            sum: function(array) {
-                var total = 0;
+    // (function($) {
+    //     $.mathUtils = {
+    //         sum: function(array) {
+    //             var total = 0;
+    //
+    //             $.each(array, function(index, value) {
+    //                 value = $.trim(value);
+    //                 value = parseFloat(value) || 0;
+    //
+    //                 total += value;
+    //             });
+    //             return total;
+    //         },
+    //         average: function(array) {
+    //             if ($.isArray(array)) {
+    //                 return $.mathUtils.sum(array) / array.length;
+    //             }
+    //             return '';
+    //         }
+    //     };
+    // })(jQuery)
+    //
+    // $(function () {
+    //
+    //     var $inventory=$('#inventory tbody');
+    //     var quantity=$inventory.find('td:nth-child(2)').map(function (index,qty) {
+    //         return $(qty).text();
+    //     }).get();
+    //     var sum= $.mathUtils.sum(quantity);
+    //     $('#sum').find('td:nth-child(2)').text(sum);
+    //
+    //     console.log(quantity);
+    //     var price=$inventory.find('td:nth-child(3)').map(function (index,qty) {
+    //         return $(qty).text();
+    //
+    //     }).get();
+    //     console.log(price);
+    //     var average=$.mathUtils.average(price)
+    //     $('#average').find('td:nth-child(3)').text(average.toFixed(2))
+    // })
 
-                $.each(array, function(index, value) {
-                    value = $.trim(value);
-                    value = parseFloat(value) || 0;
 
-                    total += value;
-                });
-                return total;
-            },
-            average: function(array) {
-                if ($.isArray(array)) {
-                    return $.mathUtils.sum(array) / array.length;
-                }
-                return '';
-            }
-        };
+    //例8-8 使用hasClass() 方法进行类的存在判断
+
+    // (function ($) {
+    //     $.fn.swapClass=function (class1,class2) {
+    //         if(this.hasClass(class1)){
+    //             this.removeClass(class1).addClass(class2)
+    //         }else if (this.hasClass(class2)){
+    //             this.removeClass(class2).addClass(class1)
+    //         }
+    //     }
+    //
+    // })(jQuery)
+    //
+    // $(function () {
+    //     $('table').click(function () {
+    //         console.log('12121212')
+    //         $('tr').swapClass('one','two')
+    //     })
+    // })
+
+
+    (function ($) {
+        $.fn.swapClass=function (class1,class2) {
+           this.each(function () {
+               var  $this=$(this);
+               if($this.hasClass(class1)){
+                   $this.removeClass(class1).addClass(class2)
+               }else if($this.hasClass(class2)){
+                   $this.removeClass(class2).addClass(class1)
+               }
+           })
+
+        }
+
     })(jQuery)
 
     $(function () {
-
-        var $inventory=$('#inventory tbody');
-        var quantity=$inventory.find('td:nth-child(2)').map(function (index,qty) {
-            return $(qty).text();
-        }).get();
-        var sum= $.mathUtils.sum(quantity);
-        $('#sum').find('td:nth-child(2)').text(sum);
-
-        console.log(quantity);
-        var price=$inventory.find('td:nth-child(3)').map(function (index,qty) {
-            return $(qty).text();
-
-        }).get();
-        console.log(price);
-        var average=$.mathUtils.average(price)
-        $('#average').find('td:nth-child(3)').text(average.toFixed(2))
+        $('table').click(function () {
+            console.log('12121212')
+            $('tr').swapClass('one','two')
+        })
     })
-
